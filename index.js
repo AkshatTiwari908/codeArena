fbe8cdbedf7e0f1fcf398c1814820c510f46b751
ghost <akshattiwari487@gmail.com>
2025-03-11 23:13:31 +0530

    .

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import connectDB from "./connectDB/connectDB.js"
+import authRoute from "./routes/auth-routes/auth-route.js"
 const app = express()
+app.use('/auth/v1',authRoute)
 app.use(express.json())
 const port = process.env.PORT
 app.listen(port,()=>{
