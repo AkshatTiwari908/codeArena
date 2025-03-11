7a1e12d11573bf879716a532a8a226ccb6e01fbf
ghost <akshattiwari487@gmail.com>
2025-03-11 20:45:06 +0530

    .

--- routes/auth-routes/auth-route.js
+++ routes/auth-routes/auth-route.js
@@ -5,9 +5,9 @@ const router = express.Router()
 router.post('/signup',signup)
 router.post('/verify-email',verifyEmail)
 router.post('/login',login)
-router.post('/logout',logout)
+router.get('/logout',logout)
 router.post('/forget-password',forgetPassword)
-router.post('/reset-password/:token',resetPassword)
+router.put('/reset-password/:token',resetPassword)
 // Google Auth To be added 
 
 router.post('/verify-phone',()=>{
