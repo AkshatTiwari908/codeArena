6f5fc47f2070b774afe0e3988583caaa1b50555b
ghost <akshattiwari487@gmail.com>
2025-04-03 11:38:33 +0530

    Cors error dealt

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ import adminContestRoute from "./routes/admin-routes/contest-route.js"
 import contestProblemRoute from "./routes/admin-routes/problem-route.js"
 import adminAuthRouth from "./routes/auth-routes/admin-auth-route.js"
 import executeCodeRoute from "./routes/code-routes/code-execute-route.js"
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
+
+const corsOptions = {
+    origin: [
+      'https://codearena-1jev.onrender.com',
+      'http://localhost:5173',
+      'http://170.70.0.87:5173',
+      'http://192.168.137.1:5173'                   
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  };
+  
+  app.use(cors(corsOptions))
 
 app.use('/auth/v1',authRoute)
 app.use('/contest',adminContestRoute)
